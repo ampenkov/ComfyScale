a44f5fcb57ed3a6dc76c92df53c6593124ec9cdc
Aleksey Penkov <ampenkov@avito.ru>
2025-05-05 15:51:38 +0300

    fix custom nodes

--- custom_nodes/ComfyScale_mocks/__init__.py
+++ custom_nodes/ComfyScale_mocks/__init__.py
@@ -9,8 +9,10 @@ class NetworkRequestMocker:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "prompt": ("STRING",),
                 "image": ("IMAGE",),
+                "text": ("STRING",),
+                "latency_mu": ("FLOAT", {"default": 15}),
+                "latency_sigma": ("FLOAT", {"default": 1}),
             }
         }
 
@@ -19,8 +21,8 @@ class NetworkRequestMocker:
     CATEGORY = "mocks/network"
 
     @ray.remote(num_returns=2)
-    def mock(self, prompt, image):
-        time.sleep(random.normalvariate(15, 5))
+    def mock(self, image, text, latency_mu, latency_sigma):
+        time.sleep(random.normalvariate(latency_mu, latency_sigma))
         return image, None
 
 
